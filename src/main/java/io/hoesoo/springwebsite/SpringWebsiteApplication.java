797d6883078853e27c0f69c23905a2beba5619dd
장회수 <hoesoo.jang@woowahan.com>
2018-11-20 00:22:19 +0900

    프로젝트 생성

---
+++ src/main/java/io/hoesoo/springwebsite/SpringWebsiteApplication.java
@@ -0,0 +1,12 @@
+package io.hoesoo.springwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsiteApplication.class, args);
+    }
+}
