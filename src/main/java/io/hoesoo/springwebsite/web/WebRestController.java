a6565f5b68a7ab222e5fce56edd8ced61172f44f
architectj <architectj@gmail.com>
2018-11-21 18:08:02 +0900

    2편 적용

--- src/main/java/io/hoesoo/springwebsite/web/WebRestController.java
+++ src/main/java/io/hoesoo/springwebsite/web/WebRestController.java
@@ -1,13 +1,32 @@
 package io.hoesoo.springwebsite.web;
 
+import io.hoesoo.springwebsite.domain.posts.PostsRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class `WebRestController {
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+
+        // 생성일 추가 코드 예제
+//        posts.setCreateDate(new LocalDate());
+//        postsRepository.save(posts);
+        postsRepository.save(dto.toEntity());
+
+
     }
 }
