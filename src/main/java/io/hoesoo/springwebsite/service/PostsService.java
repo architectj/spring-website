509a7faec91056dffe31875c99c5fb55553b6ca9
architectj <architectj@gmail.com>
2018-11-23 17:51:49 +0900

    3편 적용

--- src/main/java/io/hoesoo/springwebsite/service/PostsService.java
+++ src/main/java/io/hoesoo/springwebsite/service/PostsService.java
@@ -1,11 +1,15 @@
 package io.hoesoo.springwebsite.service;
 
 import io.hoesoo.springwebsite.domain.posts.PostsRepository;
+import io.hoesoo.springwebsite.dto.PostsMainResponseDto;
 import io.hoesoo.springwebsite.web.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @AllArgsConstructor
 @Service
@@ -15,5 +19,13 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/io/hoesoo/springwebsite/web/WebController.java
+++ src/main/java/io/hoesoo/springwebsite/web/WebController.java
@@ -1,15 +1,28 @@
 package io.hoesoo.springwebsite.web;
 
+import io.hoesoo.springwebsite.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class WebController {
 
+//    @GetMapping("/")
+//    public String main() {
+//        return "main";
+//    }
+
+
+
+
+    private PostsService postsService;
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
 }
